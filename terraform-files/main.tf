6921a2c7076a4c5228e8a986b7d4f5ee1e1360a0
praveenkumar-834 <praveenuu34@gmail.com>
2024-10-20 14:19:00 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "test-server" {
   ami = "ami-0dee22c13ea7a9a67"
   instance_type = "t2.micro"
   key_name = "mykey"
-  vpc_security_group_ids = ["sg-031c7721f036e4df7"]
+  vpc_security_group_ids = ["sg-07acd61ef375427c4"]
   connection {
      type = "ssh"
      user = "ubuntu"
