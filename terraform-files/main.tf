7e04e9c259175900eb14fbea681222e68488aba8
praveenkumar-834 <praveenuu34@gmail.com>
2024-10-20 14:05:05 +0530

    Update main.tf
    subnet id

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "test-server" {
   ami = "ami-0dee22c13ea7a9a67"
   instance_type = "t2.micro"
+  subnet_id = ["vpc-08fe6da40ab27c6b5"]
   key_name = "mykey"
-  vpc_id = ["vpc-08fe6da40ab27c6b5"]
   vpc_security_group_ids = ["sg-031c7721f036e4df7"]
   connection {
      type = "ssh"
